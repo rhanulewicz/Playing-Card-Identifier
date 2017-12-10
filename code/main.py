9e3264c6ff5d5afc697cff631976e3f0cc41d8f3
Richard Hanulewicz <rshanule@buffalo.edu>
2017-12-10 00:02:41 -0500

    Added new sample and use opening to remove noise

--- code/main.py
+++ code/main.py
@@ -5,28 +5,29 @@ import matplotlib.pyplot as plt
 img = cv2.imread('../data/input/AC_straight.jpg', cv2.IMREAD_GRAYSCALE)
 
 # First, we'll isolate the card in the scene and scrub the image
-ret,imthresh = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
-cv2.imshow('image', imthresh)
+ret,imthresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+
+# Perform opening on thresholded image to remove noise in space
+kernel = np.ones((8,8), np.uint8)
+imthresh_open = cv2.morphologyEx(imthresh, cv2.MORPH_OPEN, kernel)
 
 #Find image extents from left, right, top, and bottom
 h, w = img.shape[:2]
-
 leftextent = w;
 topextent = h;
 for i in range(1, h):
 	for j in range(1, w):
-		if imthresh[i, j] > 0:
+		if imthresh_open[i, j] > 0:
 			if i < topextent:
 				topextent = i
 			if j < leftextent:
 				leftextent = j
 				break
-
 rightextent = 1
 bottomextent = 1
 for i in range(h-1, 0, -1):
 	for j in range(w-1, 0, -1):
-		if imthresh[i, j] > 0:
+		if imthresh_open[i, j] > 0:
 			if i > bottomextent:
 				bottomextent = i
 			if j > rightextent:
@@ -37,6 +38,7 @@ print(topextent)
 print(rightextent)
 print(bottomextent)
 
+# Crop image to extents
 imgcrop = imthresh[topextent:bottomextent, leftextent:rightextent]
 cv2.imshow('imagecrop', imgcrop)
 
