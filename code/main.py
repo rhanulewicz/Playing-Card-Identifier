d3efcf698a834791b782e11e406c29d5b34341a3
Richard Hanulewicz <rshanule@buffalo.edu>
2017-12-12 20:09:43 -0500

    Error detection aglorithm set up. Doesnt work because the images aren't aligned enough.

--- code/main.py
+++ code/main.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt 
+from os import listdir
+from os.path import isfile, join
 
-img = cv2.imread('../data/input/AC_straight_rot_ext2.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('../data/input/AC_straight_rot_ext.jpg', cv2.IMREAD_GRAYSCALE)
 
-# First, we'll isolate the card in the scene and scrub the image
+# First, we'll threshold the image and make it a binary image 
 ret,imthresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 
 # Perform opening on thresholded image to remove noise in space
 kernel = np.ones((8,8), np.uint8)
 imthresh_open = cv2.morphologyEx(imthresh, cv2.MORPH_OPEN, kernel)
 
-# Get corner points using rotated rectangle
+# Get 4 corner points using rotated rectangle
 _, contours, _ = cv2.findContours(imthresh_open,2,1)
 cnt = contours[0]
 rect = cv2.minAreaRect(cnt)
@@ -23,6 +25,25 @@ cornerNE = box[2]
 cornerSW = box[0]
 cornerSE = box[3]
 
+## TODO
+# To get 4 corners of a distorted image, we can use convex hull
+hull = cv2.convexHull(cnt)
+# Get furthest points of the hull in the 8 cardinal directions
+# (Not sure the following is correct, should be verified)
+# N: 	Maximize -y
+# NE: 	Maximize x - y
+# E: 	Maximize x
+# SE: 	Maximize x + y
+# S: 	Maximize y
+# SW:	Maximize -x + y
+# W: 	Maximize -x
+# NW:	Maximize -x - y
+# The resulting hull will be an octagon
+# Take the 4 longest sides of and intersect them to find the 4 corners
+
+# Another solution might involve approxPoly but I don't know how to use it
+##
+
 # Do some homography stuff to "straighten" the card
 pts_src = np.array([cornerNW, cornerNE, cornerSW, cornerSE])
 pts_dst = np.array([[0.0, 0.0], [691, 0.0], [0.0, 1056],[691, 1056]])
@@ -31,42 +52,51 @@ homo, status = cv2.findHomography(pts_src, pts_dst)
 im_homo = cv2.warpPerspective(imthresh, homo, (im_dst.shape[1],im_dst.shape[0]))
 cv2.imshow('homo', im_homo);
 
-# If im_homo is distorted we need to "stretch" the corners until it's not
-	# Look at approxPoly for anothter solution to this
+# Isolate the card letter and symbol
+im_sym = im_homo[18:300, 18:150]
+ret2,im_sym_t = cv2.threshold(im_sym, 200, 255, cv2.THRESH_BINARY)
+# Maybe reduce symbol to bounding box?
+_, contours, _ = cv2.findContours(cv2.bitwise_not(im_sym_t),2,1)
+cnt = contours[1]
+x,y,w,h = cv2.boundingRect(cnt)
+cv2.rectangle(im_sym_t,(x,y),(x+w,y+h),(0,255,0),2)
+box = cv2.boxPoints(rect)
+box = np.int0(box)
 
-# Find image extents from left, right, top, and bottom
-h, w = img.shape[:2]
-leftextent = w;
-topextent = h;
-for i in range(1, h):
-	for j in range(1, w):
-		if imthresh_open[i, j] > 0:
-			if i < topextent:
-				topextent = i
-			if j < leftextent:
-				leftextent = j
-				break
-rightextent = 1
-bottomextent = 1
-for i in range(h-1, 0, -1):
-	for j in range(w-1, 0, -1):
-		if imthresh_open[i, j] > 0:
-			if i > bottomextent:
-				bottomextent = i
-			if j > rightextent:
-				rightextent = j
-				break
+cornerNW = box[1]
+cornerNE = box[2]
+cornerSW = box[0]
+cornerSE = box[3]
 
-# Crop image to extents
-#imgcrop = imthresh[topextent:bottomextent, leftextent:rightextent]
-#cv2.imshow('imagecrop', imgcrop)
+print(cornerNW)
+print(cornerNE)
+print(cornerSW)
+print(cornerSE)
 
-#Crop image based on corners
-#imgcrop2 = imthresh[corner1[0]:corner3[0], corner1[1]:corner2[1]]
-#h, w = imgcrop.shape[:2]
+cv2.imshow('sym', im_sym_t);
+
+# Search through all the template cards and find the one with the lowest difference
+bestError = float("inf")
+bestMatch = ""
+mypath='../data/cards_png'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+for n in range(0, len(onlyfiles)):
+	cur_img = cv2.imread( join(mypath,onlyfiles[n]), cv2.IMREAD_GRAYSCALE )
+	# Isolate letter and symbol then threshold
+	cur_sym2 = cur_img[18:300, 18:150]
+	ret4, cur_sym2_t = cv2.threshold(cur_sym2, 200, 255, cv2.THRESH_BINARY)
+	# Find difference between input card and template card
+	out = im_sym_t - cur_sym2_t
+	error = cv2.countNonZero(out)
+	if error < bestError:
+		bestError = error
+		bestMatch = onlyfiles[n]
+print(bestMatch)
+
+# Reminder on how to rotate images, in case I need it
 #rotation_matrix = cv2.getRotationMatrix2D((w/2, h/2), 90, 1)
 #img_rotation = cv2.warpAffine(imgcrop, rotation_matrix, (w, h))
-#cv2.imshow('rot', imgcrop2)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
